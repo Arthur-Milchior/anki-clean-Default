d105d213df73c43abb268e8a2ae586d24616118b
Arthur Milchior <Arthur@Milchior.fr>
2020-04-29 02:20:50 +0200

    pep8 and isort

--- cleanDefault21.py
+++ cleanDefault21.py
@@ -2,32 +2,34 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 # Feel free to contribute to this code on https://github.com/Arthur-Milchior/anki-clean-Default
 
-from aqt.utils import showWarning
 from PyQt5.QtWidgets import *
-from anki.hooks import addHook
-from aqt.qt import *
-from anki.notes import Note
+
 from anki.cards import Card
+from anki.hooks import addHook
+from anki.notes import Note
+from aqt.qt import *
 from aqt.utils import showWarning
 
-defaultID= 1
-nidToDeck={}
+defaultID = 1
+nidToDeck = {}
 """associating to nid's a non-default deck of some card. If possible, a card selected, if not, an arbitrary card."""
 
+
 def run(browser):
-    cards=[Card(browser.col,cid) for cid in browser.selectedCards()]
+    cards = [Card(browser.col, cid) for cid in browser.selectedCards()]
     for card in cards:
         prepare(card, browser)
     for card in cards:
         cleanCard(card, browser)
 
+
 def prepare(card, browser):
     """state that cid's note's deck is card's deck if it is not default"""
-    if card.did!= defaultID:
-        noteToDeck(card.nid,browser,  did=card.did)
-        
+    if card.did != defaultID:
+        noteToDeck(card.nid, browser,  did=card.did)
 
-def noteToDeck(nid,browser, did=None):
+
+def noteToDeck(nid, browser, did=None):
     """adds the did to nidToDeck. Either the given one, or one from a sibling
     card not in default.
 
@@ -37,27 +39,29 @@ def noteToDeck(nid,browser, did=None):
     if nid in nidToDeck:
         return nidToDeck[nid]
     if did:
-        nidToDeck[nid]=did
+        nidToDeck[nid] = did
         return did
 
-    note=Note(browser.col,id=nid)
+    note = Note(browser.col, id=nid)
     cards = note.cards()
     for card in cards:
         if card.did != defaultID:
-            nidToDeck[nid]=card.did
+            nidToDeck[nid] = card.did
             return card.did
     else:
         return None
 
-def cleanCard(card,browser):
+
+def cleanCard(card, browser):
     if card.did != defaultID:
         return
-    did=noteToDeck(card.nid, browser)
+    did = noteToDeck(card.nid, browser)
     if not did:
-        print("note "+str(card.nid)+" is entirely in Default")
+        print(("note "+str(card.nid)+" is entirely in Default"))
         return
     card.did = did
     card.flush()
+
 
 def setupMenu(browser):
     a = QAction("Clean Default", browser)
@@ -65,5 +69,5 @@ def setupMenu(browser):
     browser.form.menuEdit.addSeparator()
     browser.form.menuEdit.addAction(a)
 
-addHook("browser.setupMenus", setupMenu)
 
+addHook("browser.setupMenus", setupMenu)
